eaae468d934983727518af77b26629e8c88d56bf
Austin Williams <austinq.williams@gmail.com>
2019-02-17 00:03:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
  
   
 #board output
-puts " #{board[0]} | #{board[2]} | #{board[3]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
-puts " #{board[4]} | #{board[5]} | #{board[6]} "
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board[7]} | #{board[8]} | #{board[9]} "
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
